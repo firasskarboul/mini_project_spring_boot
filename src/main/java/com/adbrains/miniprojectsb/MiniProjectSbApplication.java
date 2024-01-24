7962d010a137a578d1eed91241ec534a86085dea
firasskarboul <fkarboul@gmail.com>
2024-01-24 16:00:12 +0100

    project init using Spring Initializr

---
+++ src/main/java/com/adbrains/miniprojectsb/MiniProjectSbApplication.java
@@ -0,0 +1,13 @@
+package com.adbrains.miniprojectsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjectSbApplication.class, args);
+	}
+
+}
